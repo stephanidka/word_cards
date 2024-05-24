ad065e07ae982af0e9adfd2fbe8d69b7fcedf1d1
Stepanidka <stephany.vakhotina@gmail.com>
2024-05-25 01:33:20 +0300

    problems with inputs

--- src/components/WordsTable/WordsTable.jsx
+++ src/components/WordsTable/WordsTable.jsx
@@ -3,8 +3,13 @@ import Data from '../../utils/data.json'; // Импорт данных из JSON
 import './WordsTable.css';
 import edit from '../../images/edit-ic.png';
 import bin from '../../images/bin-ico.png'
+import React, { useState } from 'react';
 
-function WordsTable(onSelect, selected) {
+const [editClicked, SetEditClicked] = useState(false)
+
+const handleEditClicked = () => SetEditClicked(!editClicked)
+
+function WordsTable() {
   return (
     <table>
       <thead className='Thead'>
@@ -19,6 +24,33 @@ function WordsTable(onSelect, selected) {
       <tbody>
         {Data.map((item, index) => (
           <tr key={index}>
+            {editClicked ? (
+                    <td><input 
+                    type="text" 
+                    value={item.word} 
+                     /></td>
+
+                     <td><input 
+                     type="text" 
+                     value={item.translation} 
+                   /></td>
+
+                   <td><input 
+                     type="text" 
+                     value={item.transcription} 
+                   /></td>
+
+                   <td><input 
+                    type="text" 
+                    value={item.theme} 
+                  /></td>
+                       <td style={{ textAlign: 'right' }}>
+                         <button>✓</button>
+                         <button>☓</button>
+                         <button><img src={edit} alt='edit' /></button>
+                         <button><img src={bin} alt='delete' /></button>
+                       </td> ) : (
+          
             <td>{item.word}</td>
             <td>{item.transcription}</td>
             <td>{item.translation}</td>
@@ -28,7 +60,7 @@ function WordsTable(onSelect, selected) {
               <button style={{display: 'none'}}>☓</button>
               <button><img src={edit} alt='edit' /></button>
               <button><img src={bin} alt='delete' /></button>
-            </td>
+            </td>)}
           </tr>
         ))}
       </tbody>
