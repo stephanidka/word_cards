be5b029278d343811b840f193c4a42cb636b873f
Stepanidka <stephany.vakhotina@gmail.com>
2024-06-27 20:59:22 +0300

    about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,3 +1,17 @@
+import './About.css';
+
 export default function About(){
-    
+    return(
+    <div className="aboutContainer">
+       <div className='aboutText'>
+        <p>Hi everyone! <br />
+       My name is Stepanida and this is an educational project, created as a result of studying at the "itgirls" programming school.
+       <p></p>
+       stephany.vakhotina@gmail.com <br />
+       Github <br />
+       Linkedin
+       </p>
+       </div>
+       </div>
+       )
 }
